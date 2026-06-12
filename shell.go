feec3f7e3046489875a2c8989126842a5a590783
白彩恋 <shiro@oom-wg.dev>
2026-06-12 14:50:28 +0800

    Update License
    Signed-off-by: 白彩恋 <shiro@oom-wg.dev>

--- shell.go
+++ shell.go
@@ -2,14 +2,14 @@
 // Copyright (C) 2016-present ShIroRRen <http://shiror.ren>.                                         =
 //                                                                                                   =
 // Part of the NGA project.                                                                          =
-// Licensed under the F2DLPR License.                                                                =
+// Licensed under the File to Downloader License.                                                    =
 //                                                                                                   =
 // YOU MAY NOT USE THIS FILE EXCEPT IN COMPLIANCE WITH THE LICENSE.                                  =
 // Provided "AS IS", WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                                   =
 // unless required by applicable law or agreed to in writing.                                        =
 //                                                                                                   =
 // For the NGA project, visit: <http://app.niggergo.work>.                                           =
-// For the F2DLPR License terms and conditions, visit: <http://license.fileto.download>.             =
+// For the File to Downloader License terms and conditions, visit: <http://license.fileto.download>. =
 //====================================================================================================
 
 package shell
